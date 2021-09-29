79db3d0c1fdcb7222b5eb7d104b7bc48ebd9950e
Daniel Seldura <daniel.seldura@gmail.com>
2021-09-29 23:30:15 +0800

    Updated USER_RETURN in schema
    the id should be returned not the password

--- user.resource/schemas.ts
+++ user.resource/schemas.ts
@@ -24,10 +24,10 @@ interface of CRUDReturn
 
 USER_DATA
 {
+      id: string
       name: string,
       age: number,
-      email: string,
-      password:string
+      email: string
 }
 
 LOGIN_SCHEMA
